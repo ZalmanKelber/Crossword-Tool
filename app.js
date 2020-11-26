ec831acd9d86407380541d557e7cc4bf8e13574b
ZalmanKelber <62047666+ZalmanKelber@users.noreply.github.com>
2020-11-25 19:14:01 -0500

    added all validation checks when squares are toggled

--- app.js
+++ app.js
@@ -4,22 +4,28 @@ const phases = {
     EDIT_TEXT: "EDIT_TEXT"
 }
 
-const state = {
+const state = { //state updated with unidrectional data flow
     phase: phases.INITIALIZED,
     size: 0,
-    legal: true,
+    legal: {
+        twoWords: true,
+        threeLetters: true,
+        undivided: true,
+        edge: true
+    },
+    totalFilled: 0,
     filledSquares: []
 }
 
-const addIndices = () => {
+const addIndices = () => { //adds appropriate numerical label to each square
     const puzzle = document.getElementById("puzzle");
     let count = 1;
     for (let i = 0; i < state.size; i++) {
         for (let j = 0; j < state.size; j++) {
             const square = document.getElementById(`grid-${i}X${j}`);
             const label = document.getElementById(`label-${i}X${j}`);
-            let toLabel = !square.classList.contains("filled");
-            if (toLabel && i != 0 && j != 0) {
+            let toLabel = !square.classList.contains("filled"); //initialize toLabel to true iff square is unfilled
+            if (toLabel && i != 0 && j != 0) { //if square is unfilled but not on top row or left column, check to see if it has a filled seuare above or to the left
                 const above = document.getElementById(`grid-${i - 1}X${j}`);
                 const beside = document.getElementById(`grid-${i}X${j - 1}`);
                 toLabel = above.classList.contains("filled") || beside.classList.contains("filled");
@@ -29,60 +35,104 @@ const addIndices = () => {
     }
 }
 
-const evaluateLegality = () => {
-    state.legal = true;
-    let foundTwoWordsError = false;
-    let foundTwoLettersError = false;
+const checkForWordLengthViolations = () => {
+    state.legal.twoWords = true; //reset booleans in state to true; if both turn to false, we can exit the loop early
+    state.legal.threeLetters = true;
     const {filledSquares} = state;
-    const twoWordsError = document.getElementById("two-words-error");
-    const twoLettersError = document.getElementById("two-letters-error");
-    
     for (let i = 0; i < state.size; i++) {
         for (let j = 1; j <= state.size; j++) {
-            if (((j == state.size || filledSquares[i][j]) && (j < 2 || filledSquares[i][j - 2]) && !filledSquares[i][j - 1]) ||
-            ((j == state.size || filledSquares[j][i]) && (j < 2 || filledSquares[j - 2][i]) && !filledSquares[j - 1][i])) {
-                state.legal = false;
-                foundTwoWordsError = true;
-                console.log("found two words error with i and j: ", i, j);
-                twoWordsError.setAttribute("style", "visibiity: default;");
+            //first check for a single unfilled square in between an edge and a filled square or between two filled squares
+            if (((j == state.size || filledSquares[i][j]) && (j < 2 || filledSquares[i][j - 2]) && !filledSquares[i][j - 1]) || //check horizontally
+            ((j == state.size || filledSquares[j][i]) && (j < 2 || filledSquares[j - 2][i]) && !filledSquares[j - 1][i])) { //check vertically
+                state.legal.twoWords = false;
             }
-            if (j >= 2) {
+            if (j >= 2) { //now check for two unfilled squares in between edges and/or filled squares
                 if (((j == state.size || filledSquares[i][j]) && (j < 3 || filledSquares[i][j - 3]) && !filledSquares[i][j - 1] && !filledSquares[i][j - 2]) ||
                 ((j == state.size || filledSquares[j][i]) && (j < 3 || filledSquares[j - 3][i]) && !filledSquares[j - 1][i] && !filledSquares[j - 2][i])) {
-                    state.legal = false;
-                    console.log("filledSquares[i]:", filledSquares[i]);
-                    console.log("found two letters error with i and j: ", i, j);
-                    foundTwoLettersError = true;
-                    twoLettersError.setAttribute("style", "visibility: default;");
+                    state.legal.threeLetters = false;
                 }
             }
-            if (foundTwoLettersError && foundTwoWordsError) {
+            if (!state.legal.twoWords && !state.legal.threeLetters) { //if we've identified both, we can exit the loop early
                 return;
             }
         } 
     }
+
 }
 
-const toggleFill = (i, j) => {
+const findFirstUnfilled = () => {
+    const {filledSquares, size} = state; 
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            if (!filledSquares[i][j]) {
+                return [i, j];
+            }
+        }
+    }
+}
+
+const checkForConnectedViolations = () => {
+    const {filledSquares, size, totalFilled} = state; 
+    const found = new Set();
+    const [i, j] = findFirstUnfilled();
+    found.add(i + "X" + j);
+    const q = [[i, j]];
+    let count = 1;
+    while (q.length > 0) { //run a DFS
+        const [x, y] = q.pop();
+        [[0,1],[0,-1],[1,0],[-1,0]].forEach(([dx, dy]) => { //check all adjacent squares to see if they're unfilled and not yet found
+            const [nx, ny] = [x + dx, y + dy];
+            if (nx >= 0 && nx < size && ny >= 0 && ny < size && !found.has(nx + "X" + ny) && !filledSquares[nx][ny]) {
+                count++;
+                found.add(nx + "X" + ny);
+                q.push([nx, ny]);
+            }
+        });
+    }
+    state.legal.undivided = count == size * size - totalFilled; //check the number of unfilled squares our DFS found with the known number
+}
+
+const checkForEdgeFilledViolation = () => {
+    const {filledSquares} = state;
+    if (filledSquares[0].every(sq => sq)) {
+        state.legal.edge = false;
+    } else if (filledSquares.every(row => row[0])) {
+        state.legal.edge = false;
+    } else {
+        state.legal.edge = true;
+    }
+}
+
+const evaluateLegality = () => { //update state variables that track legality and display or hide error messages accordingly
+    checkForWordLengthViolations();
+    checkForConnectedViolations();
+    checkForEdgeFilledViolation();
+    document.getElementById("two-words-error")
+        .setAttribute("style", `visibility: ${state.legal.twoWords ? "hidden" : "default"};`);
+    document.getElementById("two-letters-error")
+        .setAttribute("style", `visibility: ${state.legal.threeLetters ? "hidden" : "default"};`);
+    document.getElementById("divided-error")
+        .setAttribute("style", `visibility: ${state.legal.undivided ? "hidden" : "default"};`);
+    document.getElementById("edge-filled-error")
+        .setAttribute("style", `visibility: ${state.legal.edge ? "hidden" : "default"};`);
+}
+
+const toggleFill = (i, j) => { //change a square and its radial symmetric oppositte from filled to unfilled or vice versa
     const {filledSquares, size} = state;
-    const twoWordsError = document.getElementById("two-words-error");
-    const twoLettersError = document.getElementById("two-letters-error");
-    [twoWordsError, twoLettersError].forEach(el => el.setAttribute("style", "visibility: hidden;"));
     const el1 = document.getElementById(`grid-${i}X${j}`);
     const el2 = document.getElementById(`grid-${size - i - 1}X${size - j - 1}`);
-    const els = [el1, el2];
     const isFilled = filledSquares[i][j]
-    els.forEach(el => {
-        if (!isFilled) {
-            el.classList.add("filled");
-            filledSquares[i][j] = true;
-            filledSquares[size - i - 1][size - j - 1] = true;
-        } else {
-            el.classList.remove("filled");
-            filledSquares[i][j] = false;
-            filledSquares[size - i - 1][size - j - 1] = false;
-        }
-    });
+    if (!isFilled) {
+        [el1, el2].forEach(el => el.classList.add("filled"));
+        filledSquares[i][j] = true;
+        filledSquares[size - i - 1][size - j - 1] = true;
+        state.totalFilled += i == Math.floor(size / 2) && j == Math.floor(size / 2) ? 1 : 2; //determine if the toggled square is in the middle, in which case we're only changing one square
+    } else {
+        [el1, el2].forEach(el => el.classList.remove("filled"));
+        filledSquares[i][j] = false;
+        filledSquares[size - i - 1][size - j - 1] = false;
+        state.totalFilled -= i == Math.floor(size / 2) && j == Math.floor(size / 2) ? 1 : 2;
+    }
     addIndices();
     evaluateLegality();
 }
@@ -109,7 +159,7 @@ const renderBoard = () => {
     let rows = "";
     for (let i = 0; i < state.size; i++) {
         cols += " auto";
-        rows += "auto";
+        rows += " auto";
     }
     const puzzle = document.getElementById("puzzle");
     puzzle.innerHTML = "";
@@ -148,8 +198,9 @@ const renderBoard = () => {
 }
 
 const handleFinalize = e => {
+    const {twoWords, threeLetters, undivided, edge} = state.legal;
     e.preventDefault();
-    if (state.legal) {
+    if (twoWords && threeLetters && undivided && edge) { //make sure all conditions are met
         state.phase = phases.EDIT_TEXT;
         const finalize = document.getElementById("finalize");
         finalize.setAttribute("style", "display: none;");
@@ -157,14 +208,14 @@ const handleFinalize = e => {
     }
 }
 
-const handleSubmit = e => {
+const handleSubmit = e => { //make sure submission is valid or else set error message
     e.preventDefault();
     const sizeInput = document.getElementById("size-input").value;
     if (sizeInput >= 3 && sizeInput <= 55 && sizeInput % 2 == 1) {
         state.phase = phases.EDIT_GRID;
         state.size = sizeInput;
         const form = document.getElementById("form");
-        form.setAttribute("style", "display: none;");
+        form.setAttribute("style", "display: none;"); //hide form and display finalize button
         const finalize = document.getElementById("finalize");
         finalize.setAttribute("style", "display: default;");
         finalize.addEventListener("click", handleFinalize);
@@ -175,12 +226,12 @@ const handleSubmit = e => {
     }
 }
 
-const handleInputChange = () => {
+const handleInputChange = () => { //get rid of any error messages once input is changed
     const errorMessage = document.getElementById("error-message");
         errorMessage.setAttribute("style", "display: none;")
 }
 
-const initializePage = () => {
+const initializePage = () => { //add event listeners to form
     const form = document.getElementById("form");
     form.addEventListener("submit", handleSubmit);
     const inputField = document.getElementById("size-input");
