067be3af88e84a023e9a61c0e425f162c982882b
ZalmanKelber <62047666+ZalmanKelber@users.noreply.github.com>
2020-11-29 22:47:45 -0500

    centered crossword in pdf

--- js/pdf.js
+++ js/pdf.js
@@ -1,22 +1,24 @@
-console.log(html2canvas);
-
 const pdf = (() => {
 
-    const specialElementHandlers = {
-        ".label": function(element, renderer) {
-            return true;
-        }
+    const getPuzzleWidth = length => {
+        if (length >= 15) { return 140; }
+        if (length >= 11) { return 120; }
+        return length * 12;
     }
 
-    const generatePdf = (puzzle, clues) => {
-        const doc = new jsPDF();
-        console.log(doc);
-        doc.addHTML(
-            document.getElementById("container"), 
-            function () {
-            doc.save("puzzle.pdf");
-            }
-        );
+    const generatePdf = async (puzzle, clues) => {
+        const doc = new jsPDF("p", "mm", "a4");
+        const PAGE_HEIGHT = doc.internal.pageSize.getHeight();
+        const PAGE_WIDTH = doc.internal.pageSize.getWidth();
+        window.scrollTo(0, 0);
+        await html2canvas(document.getElementById("puzzle")).then(function(canvas) {
+            const imgData = canvas.toDataURL("image/jpeg", 1.5);
+            const img = new Image();
+            img.src = imgData;
+            const puzzleWidth = getPuzzleWidth(puzzle.length);
+            doc.addImage(img, "JPEG", (PAGE_WIDTH - puzzleWidth) / 2, (PAGE_HEIGHT - puzzleWidth) / 2, puzzleWidth, puzzleWidth);
+        });
+        doc.save("puzzle.pdf");
     };
 
     return { generatePdf };
